80e72ca2a6a849b8dd4e3865a76fe37e91c2a040
nuarharuha <nhnoah@gmail.com>
2012-12-10 02:16:40 +0800

    added infovis visual data graph library

---
+++ library/js/rgraph.js
@@ -0,0 +1,142 @@
+/**
+ *  Radial Graph
+ *
+ *  @package isralife
+ *
+ *  @author Nuarharuha <nhoah+bay-isra@gmail.com>
+ *  @version 1.0
+ *  @since 0.1
+ */
+var genealogy = {
+    data: false,
+    uri: '/wp-admin/admin-ajax.php'
+};
+
+var labelType, useGradients, nativeTextSupport, animate;
+
+(function() {
+    var ua = navigator.userAgent,
+        iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+        typeOfCanvas = typeof HTMLCanvasElement,
+        nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+        textSupport = nativeCanvasSupport
+            && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+    //I'm setting this based on the fact that ExCanvas provides text support for IE
+    //and that as of today iPhone/iPad current text support is lame
+    labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
+    nativeTextSupport = labelType == 'Native';
+    useGradients = nativeCanvasSupport;
+    animate = !(iStuff || !nativeCanvasSupport);
+})();
+
+var Log = {
+    elem: false,
+    write: function(text){
+        if (!this.elem)
+            this.elem = document.getElementById('log');
+        this.elem.innerHTML = text;
+        this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+    }
+};
+
+genealogy.init = function(rpcdata){
+     //init RGraph
+    var rgraph = new $jit.RGraph({
+        //Where to append the visualization
+        injectInto: 'infovis',
+        width: 800,
+        height: 600,
+        //Optional: create a background canvas that plots
+        //concentric circles.
+        background: {
+            CanvasStyles: {
+                strokeStyle: '#555'
+            }
+        },
+        //Add navigation capabilities:
+        //zooming by scrolling and panning.
+        Navigation: {
+            enable: true,
+            panning: true,
+            zooming: 10
+        },
+        //Set Node and Edge styles.
+        Node: {
+            color: '#ddeeff'
+        },
+
+        Edge: {
+            //color: '#C17878',
+            color: '#6a5acd',
+            lineWidth:1.8
+        },
+
+        onBeforeCompute: function(node){
+            Log.write("centering " + node.name + "...");
+            //Add the relation list in the right column.
+            //This list is taken from the data property of each JSON node.
+            $jit.id('inner-details').innerHTML = node.data.relation;
+        },
+
+        //Add the name of the node in the correponding label
+        //and a click handler to move the graph.
+        //This method is called once, on label creation.
+        onCreateLabel: function(domElement, node){
+            domElement.innerHTML = node.name;
+            domElement.onclick = function(){
+                rgraph.onClick(node.id, {
+                    onComplete: function() {
+                        Log.write("done");
+                    }
+                });
+            };
+        },
+        //Change some label dom properties.
+        //This method is called each time a label is plotted.
+        onPlaceLabel: function(domElement, node){
+            var style = domElement.style;
+            style.display = '';
+            style.cursor = 'pointer';
+
+            if (node._depth <= 1) {
+                style.fontSize = "0.8em";
+                style.color = "#ccc";
+
+            } else if(node._depth == 2){
+                style.fontSize = "0.7em";
+                style.color = "#494949";
+
+            } else {
+                style.display = 'none';
+            }
+
+            var left = parseInt(style.left);
+            var w = domElement.offsetWidth;
+            style.left = (left - w / 2) + 'px';
+        }
+    });
+    //load JSON data
+    rgraph.loadJSON(rpcdata);
+    //trigger small animation
+    rgraph.graph.eachNode(function(n) {
+        var pos = n.getPos();
+        pos.setc(0, 0);
+    });
+    rgraph.compute('end');
+    rgraph.fx.animate({
+        modes:['polar'],
+        duration: 2000
+    });
+    //end
+    //append information about the root relations in the right column
+    $jit.id('inner-details').innerHTML = rgraph.graph.getNode(rgraph.root).data.relation;
+}
+
+genealogy.prep = function(){
+    $.post( this.uri, {action:'rpc_tree',nonce: $('#_wpnonce').val(), uid: $('#uid').val()}, function(data){
+        if (data.success){ genealogy.init(data.tree);
+        } else { $('#log').text('Update Failed'); }
+    },'JSON');
+}
+
+jQuery(document).ready(function($){ genealogy.prep(); });
